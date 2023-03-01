91076826cbf81ebcf3a8eff350bfd0e953d5de1e
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-01 07:30:19 +0300

    Add paint with hovering effect

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ for (i = 0; i < 256; i++) {
   gridSquare.classList.add("grid-square");
   container.appendChild(gridSquare);
 }
+
+const square = document.querySelectorAll(".grid-square");
+
+square.forEach(s => {
+  s.addEventListener("mouseenter", () => {
+    s.classList.add("square");
+  });
+  //   s.addEventListener("mouseleave", () => {
+  //     s.classList.remove("square")
+  //   })
+});
