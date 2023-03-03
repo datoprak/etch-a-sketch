91d930706587260083d11278613473f3abf5c832
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-03 11:20:41 +0300

    Add click&draw instead of hovering

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const container = document.querySelector(".container");
+const defaultSize = 16;
 
-for (i = 0; i < 256; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.classList.add("grid-square");
-  container.appendChild(gridSquare);
+const grid = document.querySelector(".grid");
+const slider = document.querySelector("#slider");
+
+function setupGrid(size) {
+  grid.innerHTML = "";
+
+  for (i = 0; i < size ** 2; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    gridSquare.setAttribute(
+      "style",
+      `flex: 0 0 ${100 / size}%; height: ${640 / size}px`
+    );
+    gridSquare.addEventListener("mouseover", draw);
+    gridSquare.addEventListener("mousedown", draw);
+    grid.appendChild(gridSquare);
+  }
 }
 
-const square = document.querySelectorAll(".grid-square");
+let clicked = false;
+document.body.onmouseup = () => (clicked = false);
+document.body.onmousedown = () => (clicked = true);
 
-square.forEach(s => {
-  s.addEventListener("mouseenter", () => {
-    s.classList.add("square");
-  });
-  //   s.addEventListener("mouseleave", () => {
-  //     s.classList.remove("square")
-  //   })
-});
+function draw(e) {
+  if (e.type === "mouseover" && clicked) {
+    console.log(clicked);
+    e.target.style.backgroundColor = "black";
+  }
+}
+
+slider.onchange = e => {
+  setupGrid(e.target.value);
+  console.log(e.target.value);
+};
+
+window.onload = () => {
+  setupGrid(defaultSize);
+};
